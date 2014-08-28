60304f0fe4281c6d5d4b9c8b2f2cc9c7bfc664f9
Caleb Morris <caleb.morris.g@gmail.com>
2014-08-27 21:19:01 -0700

    Use env for config

--- config.js
+++ config.js
@@ -4,10 +4,10 @@ var store      = new Confidence.Store();
 
 var config = {
   twitter : {
-    apiKey    : 'API_KEY',
-    apiSecret : 'API_SECRET'
+    apiKey    : process.env.TWITTER_API_KEY || '',
+    apiSecret : process.env.TWITTER_API_SECRET || ''
   }
-}
+};
 store.load(config);
 
 module.exports = store;
